9bdd5dc65405726a55be122ec522f2af87c42b52
maj001 <maj001@users.noreply.github.com>
2017-02-21 14:39:37 +1100

    empty line removed, sys out updated

--- hello-world-java/src/pg/mj/helloworld/java/Main.java
+++ hello-world-java/src/pg/mj/helloworld/java/Main.java
@@ -3,7 +3,7 @@ package pg.mj.helloworld.java;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Java");
+		System.out.println("Hello World Java!");
 	}
 
 }
