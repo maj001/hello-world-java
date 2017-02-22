f969e2fab0b2f3b6bb754a5dcddffedef4e7d360
maj001 <maj001@users.noreply.github.com>
2017-02-22 11:39:39 +1100

    synchronized with github

--- hello-world-java/src/pg/mj/helloworld/java/Main.java
+++ hello-world-java/src/pg/mj/helloworld/java/Main.java
@@ -3,6 +3,7 @@ package pg.mj.helloworld.java;
 public class Main {
 
 	public static void main(String[] args) {
+		//line added from workspace1
 		System.out.println("Hello World Java!");
 		//line added from workspace2
 	}
