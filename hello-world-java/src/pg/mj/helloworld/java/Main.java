f4c658e0b6836cd6a6378f1da7d4fea3983630ca
maj001 <maj001@users.noreply.github.com>
2017-02-22 11:56:02 +1100

    merged with remote - added line added from workspace1 1154

--- hello-world-java/src/pg/mj/helloworld/java/Main.java
+++ hello-world-java/src/pg/mj/helloworld/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		//line added from workspace1
 		//line added from workspace1 1143
+		//line added from workspace1 1154
 		System.out.println("Hello World Java!");
 		//line added from workspace2
 		//line added from workspace2 1146
