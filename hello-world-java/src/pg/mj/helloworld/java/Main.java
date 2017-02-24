18a40ab78dea5c369a31b31664a3a32242dd2f49
maj001 <maj001@users.noreply.github.com>
2017-02-25 10:50:10 +1100

    comments removed

--- hello-world-java/src/pg/mj/helloworld/java/Main.java
+++ hello-world-java/src/pg/mj/helloworld/java/Main.java
@@ -3,16 +3,7 @@ package pg.mj.helloworld.java;
 public class Main {
 
 	public static void main(String[] args) {
-		//line added from workspace1
-		//line added from workspace1 1143
-		//line added from workspace1 1154
-		//line added from workspace1 1201
-		//line added from workspace1 1208
 		System.out.println("Hello World Java!");
-		//line added from workspace2
-		//line added from workspace2 1146
-		//line added from workspace2 1158
-		//line added from workspace2 1204
 	}
 
 }
